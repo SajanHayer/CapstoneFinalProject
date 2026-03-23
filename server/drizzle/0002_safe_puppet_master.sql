4a246f604657555bcb7f63e708c3aa19c8794b41
Sajan Hayer <sajanhayer@mac.ucalgary.ca>
2026-03-23 13:11:45 -0600

    Merging

--- server/drizzle/0002_safe_puppet_master.sql
+++ server/drizzle/0002_safe_puppet_master.sql
@@ -1,1 +1,3 @@
 ALTER TYPE "public"."listing_end_reason" ADD VALUE 'cancelled';
+ALTER TABLE "listings" ADD COLUMN "latitude" numeric(10, 7);--> statement-breakpoint
+ALTER TABLE "listings" ADD COLUMN "longitude" numeric(10, 7);
